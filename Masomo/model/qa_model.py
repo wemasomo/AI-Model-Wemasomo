b94ed6d587572e0b2fe799b71999ba0c04173b3d
Tania Garcia <65242509+taniabg@users.noreply.github.com>
2024-08-30 12:19:47 +0200

    qa streamlit added

--- Masomo/model/qa_model.py
+++ Masomo/model/qa_model.py
@@ -1,16 +1,21 @@
+import pandas as pd
 from transformers import pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class QuestionAnsweringModel:
-    def __init__(self, model_name="deepset/bert-large-uncased-whole-word-masking-squad2"):
-        # Cargar el pipeline de question-answering con el modelo especificado
+    def __init__(self, model_name="deepset/roberta-base-squad2"):
         self.qa_pipeline = pipeline("question-answering", model=model_name)
 
-    def answer_question(self, question: str, context: str) -> str:
-        """
-        Responde a una pregunta basada en el contexto proporcionado.
-        :param question: Pregunta que necesita una respuesta.
-        :param context: Texto del cual se extraerá la respuesta.
-        :return: Respuesta generada por el modelo.
-        """
+    def find_most_relevant_text(self, question, df):
+        texts = df['Text'].tolist()
+        texts.append(question)
+        vectorizer = TfidfVectorizer().fit_transform(texts)
+        vectors = vectorizer.toarray()
+        cosine_similarities = cosine_similarity([vectors[-1]], vectors[:-1]).flatten()
+        best_index = cosine_similarities.argmax()
+        return df['Text'].iloc[best_index]
+
+    def answer_question(self, question, context):
         result = self.qa_pipeline(question=question, context=context)
         return result['answer']
